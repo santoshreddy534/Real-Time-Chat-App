fab41f35d7ffd1e595864f477cbc3af9272d65b6
Santosh Reddy <santu.santosh16@gmail,com>
2023-10-05 12:14:23 -0500

    Developed Chat API, Message API, fetched chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,6 +1,29 @@
+import { useContext } from "react";
+import {Container, Stack} from "react-bootstrap";
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
+import UserChat from "../components/chat/userChat";
+
 const Chat =()=>{
+  const {user} = useContext(AuthContext);
+  const {userChats, isUserChatsLoading, userChatsError} = useContext(ChatContext);
   return (
-    <div>Chat</div>
+    <Container>{userChats?.length < 1 ? null : 
+    (<Stack>
+      <Stack direction="horizontal" gap={4} className="align-items-start">
+        <Stack className=" messages-box flex-grow-0 pe-3" gap={3}>
+        {isUserChatsLoading && <p>Loading chats...</p>}
+        {userChats?.map((chat, index)=>{
+          return(
+            <div key={index}>
+              <UserChat chat={chat} user={user} />
+            </div>
+          )
+        })}
+        </Stack>
+        <p>ChatBox</p>
+      </Stack>
+    </Stack>)}</Container>
   )
 }
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const userRoute = require("./Routes/userRoute")
+const chatRoute = require("./Routes/chatRoute")
+const messageRoute = require("./Routes/messageRoute")
 
+//Middlewares
 app.use(express.json());
 app.use(cors());
-
 app.use("/api/user", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.get("/", (req, res)=>{
     res.send("Welcome to our chat app APIs..")
